264179f4da64bf92300bafcea5c956469ed2d81c
JorgenBrattang <jorgen.kvist@gmail.com>
2022-07-04 12:05:33 +0000

    Add core structure for function calculateCorrectAnswer

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,7 +39,7 @@ function runGame(gameType) {
     } else {
         // If it gameType doesn't exist throw errors!
         alert(`Unknown game type: ${gameType}`);
-        throw `Unknown game type: ${gameType}. Aborting!`
+        throw `Unknown game type: ${gameType}. Aborting!`;
     }
 }
 
@@ -47,8 +47,25 @@ function checkAnswer() {
     
 }
 
+/**
+ * Gets the operands (the numbers) and the operator (plus, minus etc)
+ * directly from the DOM, and return the correct answer.
+ */
 function calculateCorrectAnswer() {
-    
+
+    // Gets the string values from the DOM, and converts the operands to integers with parseInt();
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById('operator').innerText;
+
+    // Checks if the operator is a plus (+), if not throw an alert
+    if (operator === '+') {
+        return [operand1 + operand2, 'addition'];
+    } else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting!`
+    }
+
 }
 
 function incrementScore() {
